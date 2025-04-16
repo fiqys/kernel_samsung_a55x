49e0398380d3cdf08c3cb2cfed6a9e50a437f8fd
mst8981 <unformed8981@gmail.com>
2025-04-17 04:16:53 +0800

    printk: Add PRINTK_PROCESS for per-message process info
    Add PRINTK_PROCESS config to allow printk messages to include
    CPU ID, process name, and PID.

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -384,7 +384,11 @@ static struct latched_seq clear_seq = {
 	.val[1]		= 0,
 };
 
-#ifdef CONFIG_PRINTK_CALLER
+#if defined(CONFIG_PRINTK_CALLER) && defined(CONFIG_PRINTK_PROCESS)
+#define PREFIX_MAX		72
+#elif defined(CONFIG_PRINTK_PROCESS)
+#define PREFIX_MAX		58
+#elif defined(CONFIG_PRINTK_CALLER)
 #define PREFIX_MAX		48
 #else
 #define PREFIX_MAX		32
@@ -1315,6 +1319,15 @@ static size_t print_caller(u32 id, char *buf)
 #else
 #define print_caller(id, buf) 0
 #endif
+#ifdef CONFIG_PRINTK_PROCESS
+static size_t print_process(const struct printk_info *info, char *buf)
+{
+	return sprintf(buf, "%c[%1d:%15s:%5d]", info->in_interrupt ? 'I' : ' ',
+			info->cpu, info->process, info->pid);
+}
+#else
+#define print_process(info, buf) 0
+#endif
 
 static size_t info_print_prefix(const struct printk_info  *info, bool syslog,
 				bool time, char *buf)
@@ -1328,8 +1341,9 @@ static size_t info_print_prefix(const struct printk_info  *info, bool syslog,
 		len += print_time(info->ts_nsec, buf + len);
 
 	len += print_caller(info->caller_id, buf + len);
+	len += print_process(info, buf + len);
 
-	if (IS_ENABLED(CONFIG_PRINTK_CALLER) || time) {
+	if (IS_ENABLED(CONFIG_PRINTK_CALLER) || IS_ENABLED(CONFIG_PRINTK_PROCESS) || time) {
 		buf[len++] = ' ';
 		buf[len] = '\0';
 	}
@@ -2292,6 +2306,13 @@ int vprintk_store(int facility, int level,
 	r.info->flags = flags & 0x1f;
 	r.info->ts_nsec = ts_nsec;
 	r.info->caller_id = caller_id;
+#ifdef CONFIG_PRINTK_PROCESS
+	strncpy(r.info->process, current->comm, sizeof(r.info->process) - 1);
+	r.info->process[sizeof(r.info->process) - 1] = '\0';
+	r.info->pid = task_pid_nr(current);
+	r.info->cpu = smp_processor_id();
+	r.info->in_interrupt = in_interrupt() ? 1 : 0;
+#endif
 	if (dev_info)
 		memcpy(&r.info->dev_info, dev_info, sizeof(r.info->dev_info));
 
